9d1e704713af9c34a1bc2fd0029ba5c6272ba689
huggla <henrik.uggla@kristianstad.se>
2018-03-31 18:24:17 +0200

    Update x01.cache.sh

--- initdb/x01.cache.sh
+++ initdb/x01.cache.sh
@@ -8,7 +8,7 @@ then
    read USER_PASSWORD < "$USER_PASSWORD_FILE"
 fi
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
    CREATE USER "$USER" WITH LOGIN NOINHERIT VALID UNTIL 'infinity' PASSWORD '$USER_PASSWORD';
    CREATE DATABASE "$DATABASE" WITH OWNER = "$POSTGRES_USER" TEMPLATE=template_postgis;
 EOSQL
@@ -18,7 +18,7 @@ then
    read FOREIGN_SERVER_USER_PASSWORD < "$FOREIGN_SERVER_USER_PASSWORD_FILE"
 fi
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$DATABASE" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "postgres" "$DATABASE" <<-EOSQL
     CREATE EXTENSION postgres_fdw;
     CREATE SERVER "$FOREIGN_SERVER_NAME" FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host '$FOREIGN_SERVER_ADDRESS', dbname '$FOREIGN_SERVER_DATABASE', port '$FOREIGN_SERVER_PORT');
     ALTER SERVER "$FOREIGN_SERVER_NAME" OPTIONS (ADD updatable 'false');
@@ -36,22 +36,22 @@ do
       limitstr=""
    fi
    ftable_schema=$fschema"_foreign"
-   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$DATABASE" <<-___EOSQL
-      CREATE SCHEMA $ftable_schema AUTHORIZATION "$POSTGRES_USER";
+   psql -v ON_ERROR_STOP=1 --username "postgres" "$DATABASE" <<-___EOSQL
+      CREATE SCHEMA $ftable_schema AUTHORIZATION "postgres";
       GRANT USAGE ON SCHEMA $ftable_schema TO "$USER";
       ALTER DEFAULT PRIVILEGES IN SCHEMA $ftable_schema GRANT SELECT ON TABLES TO "$USER";
       IMPORT FOREIGN SCHEMA "$fschema" $limitstr FROM SERVER "$FOREIGN_SERVER_NAME" INTO $ftable_schema;
-      CREATE SCHEMA "$fschema" AUTHORIZATION "$POSTGRES_USER";
+      CREATE SCHEMA "$fschema" AUTHORIZATION "postgres";
       GRANT USAGE ON SCHEMA "$fschema" TO "$USER";
       ALTER DEFAULT PRIVILEGES IN SCHEMA "$fschema" GRANT SELECT ON TABLES TO "$USER";
 ___EOSQL
    if [ -z "$foreign_server_schema_tables" ]
    then
-      foreign_server_schema_tables=`psql -q -A -t -R , -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$DATABASE" -c "SELECT table_name FROM information_schema.tables WHERE table_schema='$ftable_schema'"`
+      foreign_server_schema_tables=`psql -q -A -t -R , -v ON_ERROR_STOP=1 --username "postgres" "$DATABASE" -c "SELECT table_name FROM information_schema.tables WHERE table_schema='$ftable_schema'"`
    fi   
    for ftable in $foreign_server_schema_tables
    do
-      psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" "$DATABASE" -c "CREATE MATERIALIZED VIEW $fschema.$ftable AS SELECT * FROM $ftable_schema.$ftable WITH DATA;"
+      psql -v ON_ERROR_STOP=1 --username "postgres" "$DATABASE" -c "CREATE MATERIALIZED VIEW $fschema.$ftable AS SELECT * FROM $ftable_schema.$ftable WITH DATA;"
    done
 done
 eval $ADDITIONAL_CONFIGURATION
