83386b59ca9639ea8d1f2d6d68c1375ed8fd6dd7
huggla <henrik.uggla@kristianstad.se>
2018-04-04 09:33:51 +0200

    Update x01.cache.sh

--- initdb/x01.cache.sh
+++ initdb/x01.cache.sh
@@ -13,10 +13,10 @@ fi
 readonly USER_PASSWORD
 readonly DATABASE="$(var - DATABASE)"
 #psql_cmd="/usr/bin/env -i $BIN_DIR/sudo -u $NAME $BIN_DIR/psql -v ON_ERROR_STOP=1 --username $NAME --dbname $NAME"
-eval $psql_cmd <<-EOSQL
-   CREATE USER "$USER" WITH LOGIN NOINHERIT VALID UNTIL 'infinity' PASSWORD '$USER_PASSWORD';
-   CREATE DATABASE "$DATABASE" WITH OWNER = "postgres";
-EOSQL
+#eval $psql_cmd <<-EOSQL
+echo "CREATE USER \"$USER\" WITH LOGIN NOINHERIT VALID UNTIL 'infinity' PASSWORD '$USER_PASSWORD';" > $0.sql
+echo "CREATE DATABASE \"$DATABASE\" WITH OWNER = \"postgres\";" >> $0.sql
+#EOSQL
 exit
 # TEMPLATE=template_postgis;
 readonly FOREIGN_SERVER_USER_PASSWORD_FILE="$(var - FOREIGN_SERVER_USER_PASSWORD_FILE)"
