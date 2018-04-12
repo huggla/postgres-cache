cb22ca67fd40281a11b41a1d6fbfd29a94ed5ac2
huggla <henrik.uggla@kristianstad.se>
2018-04-12 10:43:33 +0200

    Update 040.cache.sh

--- initdb/040.cache.sh
+++ initdb/040.cache.sh
@@ -25,24 +25,15 @@ sql_file="$BIN_DIR/initdb/$prio.$dbname.sql"
 IFS=$(echo -en ",")
 for fschema in $FOREIGN_SERVER_SCHEMAS
 do
-   fschema="$(trim "$fschema")"
-   foreign_server_schema_tables="$(var - $fschema)"
+#   fschema="$(trim "$fschema")"
+#   foreign_server_schema_tables="$(var - $fschema)"
    if [ -n "$foreign_server_schema_tables" ]
    then 
       limitstr="LIMIT TO ($foreign_server_schema_tables)"
    else
       limitstr=""
    fi
-   ftable_schema=$fschema"_foreign"
-   {
-      echo "CREATE SCHEMA $ftable_schema AUTHORIZATION \"postgres\";"
-      echo "GRANT USAGE ON SCHEMA $ftable_schema TO \"$USER\";"
-      echo "ALTER DEFAULT PRIVILEGES IN SCHEMA $ftable_schema GRANT SELECT ON TABLES TO \"$USER\";"
-      echo "IMPORT FOREIGN SCHEMA \"$fschema\" $limitstr FROM SERVER \"$FOREIGN_SERVER_NAME\" INTO $ftable_schema;"
-      echo "CREATE SCHEMA \"$fschema\" AUTHORIZATION \"postgres\";"
-      echo "GRANT USAGE ON SCHEMA \"$fschema\" TO \"$USER\";"
-      echo "ALTER DEFAULT PRIVILEGES IN SCHEMA \"$fschema\" GRANT SELECT ON TABLES TO \"$USER\";"
-   } >> "$sql_file"
+
 done
 
    if [ -z "$foreign_server_schema_tables" ]
