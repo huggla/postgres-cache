6b9052b45074931d6a4ef71b5635e6f28ae9c2aa
Henrik Uggla <henrik.uggla@kristianstad.se>
2018-05-18 14:10:53 +0200

    Update 035.cache.sh

--- initdb/035.cache.sh
+++ initdb/035.cache.sh
@@ -6,18 +6,15 @@
 # . /start/stage2.functions
 # VAR_*
 
-IFS_tmp=$IFS
-IFS=$(echo -en " ")
-FOREIGN_SERVER_SCHEMAS="$(var - FOREIGN_SERVER_SCHEMAS)"
 prio="035"
-dbname="$DATABASE"
-sql_file="$CONFIG_DIR/initdb/$prio.$dbname.sql"
+dbname="$VAR_DATABASE"
+sql_file="/initdb/$prio.$dbname.sql"
 >"$sql_file"
 IFS=$(echo -en ",")
-for fschema in $FOREIGN_SERVER_SCHEMAS
+for fschema in $VAR_FOREIGN_SERVER_SCHEMAS
 do
    fschema="$(trim "$fschema")"
-   foreign_server_schema_tables="$(var - $fschema)"
+   foreign_server_schema_tables=$(echo \$VAR_$fschema)
    if [ -n "$foreign_server_schema_tables" ]
    then 
       limitstr="LIMIT TO ($foreign_server_schema_tables)"
@@ -26,13 +23,12 @@ do
    fi
    ftable_schema=$fschema"_foreign"
    {
-      echo "CREATE SCHEMA $ftable_schema AUTHORIZATION \"postgres\";"
-      echo "GRANT USAGE ON SCHEMA $ftable_schema TO \"$USER\";"
-      echo "ALTER DEFAULT PRIVILEGES IN SCHEMA $ftable_schema GRANT SELECT ON TABLES TO \"$USER\";"
-      echo "IMPORT FOREIGN SCHEMA \"$fschema\" $limitstr FROM SERVER \"$FOREIGN_SERVER_NAME\" INTO $ftable_schema;"
-      echo "CREATE SCHEMA \"$fschema\" AUTHORIZATION \"postgres\";"
-      echo "GRANT USAGE ON SCHEMA \"$fschema\" TO \"$USER\";"
-      echo "ALTER DEFAULT PRIVILEGES IN SCHEMA \"$fschema\" GRANT SELECT ON TABLES TO \"$USER\";"
+      echo "CREATE SCHEMA $ftable_schema AUTHORIZATION \"$VAR_LINUX_USER\";"
+      echo "GRANT USAGE ON SCHEMA $ftable_schema TO \"$VAR_USER\";"
+      echo "ALTER DEFAULT PRIVILEGES IN SCHEMA $ftable_schema GRANT SELECT ON TABLES TO \"$VAR_USER\";"
+      echo "IMPORT FOREIGN SCHEMA \"$fschema\" $limitstr FROM SERVER \"$VAR_FOREIGN_SERVER_NAME\" INTO $ftable_schema;"
+      echo "CREATE SCHEMA \"$fschema\" AUTHORIZATION \"$VAR_LINUX_USER\";"
+      echo "GRANT USAGE ON SCHEMA \"$fschema\" TO \"$VAR_USER\";"
+      echo "ALTER DEFAULT PRIVILEGES IN SCHEMA \"$fschema\" GRANT SELECT ON TABLES TO \"$VAR_USER\";"
    } >> "$sql_file"
 done
-IFS=$IFS_tmp
